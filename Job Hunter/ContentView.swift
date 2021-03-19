dca7520aaba4c693acab2cc6414f37b92ad134fd
jerry <jerrylai19990120@gmail.com>
2021-03-19 14:43:36 -0400

    Initial Commit

---
+++ Job Hunter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Job Hunter
+//
+//  Created by Jerry Lai on 2021-03-19.
+//  Copyright © 2021 Jerry Lai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
